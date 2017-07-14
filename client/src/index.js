3eabc91dce288853b86ac3f60b828963ee102c58
j8298c <juliocmojica@gmail.com>
2017-07-14 10:07:27 -0400

    removed client folder

--- client/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import WorkoutContainer from './components/Workouts/WorkoutContainer';
-
-ReactDOM.render(<WorkoutContainer />, document.getElementById('root'));
