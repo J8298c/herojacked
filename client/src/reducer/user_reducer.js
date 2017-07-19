f21d77ab52f16578b5f24079fff9285f3d529d3c
juliocmojica@gmail.com <juliocmojica@gmail.com>
2017-07-19 11:18:56 -0400

    added user reducer and if statement

---
+++ client/src/reducer/user_reducer.js
@@ -0,0 +1,28 @@
+import { LOGIN_USER, SIGNUP_USER } from '../actions/index';
+import store from '../store.js';
+
+let user = {
+  email: '',
+  password: '',
+};
+
+const userReducer = (state = store.getState(), action) => {
+  if (action.type === LOGIN_USER) {
+    const { email, password } = action;
+    user = {
+      email,
+      password,
+    };
+    return user;
+  } else if (action.type === SIGNUP_USER) {
+    const { email, password } = action;
+    user = {
+      email,
+      password,
+    };
+    return user;
+  }
+  return state;
+};
+
+export default userReducer;
