862c6fd671c279a49f9749af8b21ddd9ca7ae810
j8298c <juliocmojica@gmail.com>
2017-07-24 12:11:45 -0400

    added header component to AddWorkout Component

--- client/src/Components/AddWorkout/AddWorkout.js
+++ client/src/Components/AddWorkout/AddWorkout.js
@@ -3,6 +3,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Form } from 'semantic-ui-react';
 import { addingWorkout } from '../../actions/index';
+import PageHeader from '../PageHeader/PageHeader';
 import Input from '../Input';
 import AppButton from '../Button';
 import './AddWorkout.css';
@@ -25,31 +26,34 @@ class AddWorkout extends Component {
   }
   render() {
     return (
-      <Form>
-        <Input
-          type="text" name="name" className="createinput" htmlFor="name"
-          label="WorkoutName"
-          onChange={e => this.setState({ name: e.target.value })}
-        />
-        <Input
-          type="text" name="reps" className="createinput" htmlFor="reps"
-          label="Reps"
-          onChange={e => this.setState({ reps: Number(e.target.value) })}
-        />
-        <Input
-          type="text" name="sets" className="createinput" htmlFor="sets"
-          label="Sets"
-          onChange={e => this.setState({ sets: Number(e.target.value) })}
-        />
-        <Input
-          type="text" name="bodyParts" className="createinput" htmlFor="bodyParts"
-          label="BodyParts"
-          onChange={e => this.setState({ bodyParts: e.target.value })}
-        />
-        <AppButton
-          content="Save" className="createButton" onClick={this.createWorkout}
-        />
-      </Form>
+      <div>
+        <PageHeader text='Add A Workout'/>
+        <Form>
+          <Input
+            type="text" name="name" className="createinput" htmlFor="name"
+            label="WorkoutName"
+            onChange={e => this.setState({ name: e.target.value })}
+          />
+          <Input
+            type="text" name="reps" className="createinput" htmlFor="reps"
+            label="Reps"
+            onChange={e => this.setState({ reps: Number(e.target.value) })}
+          />
+          <Input
+            type="text" name="sets" className="createinput" htmlFor="sets"
+            label="Sets"
+            onChange={e => this.setState({ sets: Number(e.target.value) })}
+          />
+          <Input
+            type="text" name="bodyParts" className="createinput" htmlFor="bodyParts"
+            label="BodyParts"
+            onChange={e => this.setState({ bodyParts: e.target.value })}
+          />
+          <AppButton
+            content="Save" className="createButton" onClick={this.createWorkout}
+          />
+        </Form>
+      </div>
     );
   }
 }
