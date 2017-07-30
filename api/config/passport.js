139c2f4dc58952426ce4809e167cdf3fb63a3d0b
j8298c <juliocmojica@gmail.com>
2017-07-30 11:10:01 -0400

    refactored passport file to use async and await

--- api/config/passport.js
+++ api/config/passport.js
@@ -22,18 +22,12 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true,
     }, 
-    (accessToken, refreshToken, profile, done) => {
-      /*
-        Create var to use to check and see if user exists if user doesnt exist 
-        we will create a new one.
-      */
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-          if (existingUser) {
-            done(null, existingUser);
-          } else {
-            new User({ googleId: profile.id }).save()
-              .then(user => done(null, user));
-          }
-        });
-    }));
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      } 
+      const user = await new User({ googleId: profile.id }).save()
+      done(null, user);
+    }
+  ));
