3eba57b575905061319f65b6d416e2bce16c6dac
j8298c <juliocmojica@gmail.com>
2017-08-01 14:42:41 -0400

    added prefix /api to logout and get current user method

--- api/routes/auth_routes.js
+++ api/routes/auth_routes.js
@@ -7,13 +7,12 @@ module.exports = (app) => {
 
   app.get('/auth/google/callback', passport.authenticate('google'));
 
-  app.get('/logout', (req, res) => {
+  app.get('/api/logout', (req, res) => {
     req.logout();
     res.send(req.user);
   });
 
-  app.get('/current_user', (req, res) => {
+  app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
 };
-
