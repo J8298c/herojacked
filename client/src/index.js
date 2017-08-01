01d23112c7682aa3f9689e172fa4b1cfce7fddd5
j8298c <juliocmojica@gmail.com>
2017-07-31 22:39:06 -0400

    need to add api to all routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './Store';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Routes from './routes';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+    <Routes />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
