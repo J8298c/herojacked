9a1059ce6c0d9f5733cf9b6c6f7598d34601140d
j8298c <juliocmojica@gmail.com>
2017-08-03 15:35:26 -0400

    added login with google button to landing page

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import WorkoutListContainer from './components/WorkoutList/WorkoutListContainer';
 import WorkoutContainer from './components/Workout/WorkoutContainer';
-import LandingPage from './components/LandingPage';
+import LandingPage from './components/LandingPage/LandingPage';
+import WelcomeScreen from './components/WelcomeScreen/WelcomeScreen';
 
-class Routes extends Component {
-    render() {
-        return (
-            <Router>
-                <div>
-                    <Route exact path="/" component={LandingPage} />
-                    <Route path="/allworkouts" component={WorkoutListContainer} />
-                    <Route path="/workout/:name" compnent={WorkoutContainer} />
-                </div>
-            </Router>
-        )
-    }
-};
-
+const Routes = () => (
+    <Router>
+        <div>
+         <Route exact path="/" component={LandingPage} />
+            <Route path="/allworkouts" component={WorkoutListContainer} />
+            <Route path="/workout/:name" compnent={WorkoutContainer} />
+            <Route path="/welcome" component={WelcomeScreen} />
+        </div>
+    </Router>
+);
 export default Routes;
-
-
-
-
-
--- client/src/components/LandingPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const LandingPage = () => (
-  <h1>ThunderHammer</h1>
-);
-
-export default LandingPage;
