01f59daae9886e2e22a50d6efee10cf63d2d201d
j8298c <juliocmojica@gmail.com>
2017-08-05 13:53:36 -0400

    adding workout view component

--- client/src/components/Workout/WorkoutContainer.js
+++ client/src/components/Workout/WorkoutContainer.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchingAWorkout } from '../../actions/index';
-import Loading from '../Loader';
+// import Loading from '../Loader';
 // import Workout from './Workout';
 
 let workoutView;
 class WorkoutContainer extends Component {
-  componentWillMount(){
+  componentWillMount() {
     const { name } = this.props.match.params;
     this.props.fetchingAWorkout(name);
   }
   render(props) {    
     const { workout } = this.props;
     if (workout !== null) {
-        workoutView = 'workout is readty to render';
+      workoutView = 'workout is readty to render';
     } else {
-        workoutView = 'still not ready';
+      workoutView = 'still not ready';
     }
     return (
         <div>
