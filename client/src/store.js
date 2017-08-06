ed218c86ff799dc69f2b0a10ef822efe9cf79ce8
j8298c <juliocmojica@gmail.com>
2017-08-05 22:27:31 -0400

    adding react-mdl css

--- client/src/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
-import workoutReducer from './reducers/index';
-
-const store = createStore(workoutReducer, {}, applyMiddleware(reduxThunk));
-
-export default store;
