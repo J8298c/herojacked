35ef72ac71574944687b0fcedbe27f69f84213d0
j8298c <juliocmojica@gmail.com>
2017-08-13 16:33:04 -0400

    turning landingpage into class to check for user

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Icon } from 'semantic-ui-react';
+import axios from 'axios';
 import './landingpage.css';
 import weight from '../../images/weights-icon.svg';
 
-const LandingPage = () => (
-    <div className="landingpagecontainer">
-        <h1>HeroJacked</h1>
-        <img src={weight} alt="dumbbell icon" />
-        <Button
+class LandingPage extends Component {
+  componentDidMount() {
+    axios
+    .get('/api/current_user')
+    .then(response => { console.log(response, 'the response'); })
+    .catch(err => { console.log(err); });
+  }
+  render() {
+    return (
+      <div className="landingpagecontainer">
+          <h1>HeroJacked</h1>
+          <img src={weight} alt="dumbbell icon" />
+          <Button
             color='blue'
             animated='fade'
             className='signin'
-            >
-            <Button.Content visible>
-            Sign in With Google
-            </Button.Content>
-            <Button.Content hidden>
-                <Icon
+          >
+              <Button.Content visible>
+              Sign in With Google
+              </Button.Content>
+              <Button.Content hidden>
+                  <Icon
                     name='google'
-                />
-            </Button.Content>
-        </Button>
-    </div>
-
-);
+                  />
+              </Button.Content>
+          </Button>
+      </div>
+    );
+  }
+}
 export default LandingPage;
