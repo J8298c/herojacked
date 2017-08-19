4df4e1def8e6261a13adccc952520a04caad6aa7
j8298c <juliocmojica@gmail.com>
2017-08-19 11:50:38 -0400

    adding mongodburi to env var

--- test/workout.test.js
+++ test/workout.test.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV = 'test';
 const Workouts = require('../api/models/workout_model');
-
+//need to add auth test
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../app');
