e03483d4da830acc37a00a4294f33727e5435239
j8298c <juliocmojica@gmail.com>
2017-08-18 21:43:04 -0400

    turned the routes component to class

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
 import WorkoutContainer from './components/Workout/WorkoutContainer';
@@ -6,16 +6,27 @@ import WorkoutsContainer from './components/Workouts/WorkoutsContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import CreateWorkoutContainer from './components/CreateWorkout/CreateWorkoutContainer';
 
-const Routes = () => (
-    <Router>
+/*
+  if (user) {
+    browserHistory.push('/restaurants');
+  } else {
+    browserHistory.replace('/');
+  }
+ */
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
         <div>
-            <Route exact path='/' component={LandingPage} />
-            <Route path='/allworkouts' component={WorkoutsContainer} />
-            <Route path='/workout/:name' component={WorkoutContainer} />
-            <Route path='/profile' component={ProfileContainer} />
-            <Route path='/create_workout' component={CreateWorkoutContainer} />
+          <Route exact path='/' component={LandingPage} />
+          <Route path='/allworkouts' component={WorkoutsContainer} />
+          <Route path='/workout/:name' component={WorkoutContainer} />
+          <Route path='/profile' component={ProfileContainer} />
+          <Route path='/create_workout' component={CreateWorkoutContainer} />
         </div>
-    </Router>
-);
+      </Router>
+    );
+  }
+}
 
 export default Routes;
