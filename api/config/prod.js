40fb7a50a5156205f0765a5b961636718db6e55e
j8298c <juliocmojica@gmail.com>
2017-08-19 11:47:21 -0400

    adding mongodburi to env var

--- api/config/prod.js
+++ api/config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  dbUrl: process.env.MONGO_URI,
+  dbUrl: process.env.MONGODB_URI,
   cookieKey: process.env.COOKIE_KEY,
 };
