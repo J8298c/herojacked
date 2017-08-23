7411b883bcc9b945b9340650bb111608b97c4909
j8298c <juliocmojica@gmail.com>
2017-08-23 11:15:05 -0400

    AppIcon test added

---
+++ client/src/components/AppIcon.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { expect } from 'chai';
+import { Image } from 'semantic-ui-react';
+import AppIcon from './AppIcon';
+
+describe('<AppIcon />', () => {
+  it('should render the appIcon component', () => {
+    const appIcon = mount(<AppIcon />); //might need props
+    expect(appIcon.find(Image)).to.have.length(1);
+  });
+});
