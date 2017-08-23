d6a60f09a2867a7bd5e2422d6c2b3f1ca875948e
j8298c <juliocmojica@gmail.com>
2017-08-23 07:58:17 -0400

    adding comments to user model

--- api/models/user_model.js
+++ api/models/user_model.js
@@ -1,5 +1,9 @@
+/*
+ The user schema gets most of its info from the google auth
+ the String for current_workout will query the db using the 
+ the String as the name to the Hero workout
+*/
 const mongoose = require('mongoose');
-
 
 const Schema = mongoose.Schema;
 
