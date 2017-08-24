403ad50ef1c674b92cea7164a9f7ca2e58603c1e
j8298c <juliocmojica@gmail.com>
2017-08-23 21:02:06 -0400

    adding searchbar

---
+++ client/src/components/AppSearch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Search } from 'semantic-ui-react';
+
+const AppSearch = (props) => (
+    <Search 
+      icon='search' 
+    />
+);
+
+export default AppSearch;
+
--- client/src/components/WelcomeScreen/WelcomeScreen.js
+++ client/src/components/WelcomeScreen/WelcomeScreen.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { Icon } from 'semantic-ui-react';
+import AppSearch from '../AppSearch';
 import WelcomeScreenItem from './WelcomeScreenItem';
 import './welcomescreen.css';
 
 class WelcomeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+    };
+  }
   render() {
+    console.log(this.state, 'the state');
     return (
             <div>
-                <div className='welcomeImageContainer' />
+                <div className='welcomeImageContainer'>
+                  <AppSearch onInputChange={(e) => { this.setState({ query: e.target.value }); }} />
+                  </div>
                 <WelcomeScreenItem
                   linkTo='/profile' iconName='user' iconText='Profile'
                 />
